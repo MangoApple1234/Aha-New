c58b6bc1fd197cfbc41a80c466eaa8d14b8862b8
Tuhin Kanti Pal <me@mailtuhin.ml>
2020-10-11 19:01:26 +0530

    From development machine to github

---
+++ README.md
@@ -0,0 +1,78 @@
+## Unofficial API for <tt>https://www.aha.video/</tt><br><br>
+
+### Works only for movies (Will support web series soon)<br><br>
+
+#### **API Endpoints (method get):**<br><br>
+*Request:*
+
+```bash
+https://aha.hin.life/movie?query=https://www.aha.video/movies/android-kattappa
+```
+
+*Response:*
+
+```json
+{
+   "title":"Android Kattappa",
+   "poster":"https://snagfilms-a.akamaihd.net/38c1e2aa-64c1-41c3-8b5e-674247d490c8/images/2020/10/9/1602227407120_androidkattappahdmovieonline1070x1585_3x4Images.jpg",
+   "download_270p":"https://ahatvmp4ns.akamaized.net/aha-tv/Renditions/20201007/ANDROID_KATTAPPA/ANDROID_KATTAPPA_270.mp4?__token__=exp=1602434429~acl=/*~hmac=618751710dfe3d58a9ef241e4608f5ddfad9de62895f026fe3254e341debd8bc",
+   "download_360p":"https://ahatvmp4ns.akamaized.net/aha-tv/Renditions/20201007/ANDROID_KATTAPPA/ANDROID_KATTAPPA_360.mp4?__token__=exp=1602434429~acl=/*~hmac=618751710dfe3d58a9ef241e4608f5ddfad9de62895f026fe3254e341debd8bc",
+   "download_720p":"https://ahatvmp4ns.akamaized.net/aha-tv/Renditions/20201007/ANDROID_KATTAPPA/ANDROID_KATTAPPA_720.mp4?__token__=exp=1602434429~acl=/*~hmac=618751710dfe3d58a9ef241e4608f5ddfad9de62895f026fe3254e341debd8bc",
+   "streaming_link":"https://ahatvhlsns.akamaized.net/aha-tv/Renditions/20201007/ANDROID_KATTAPPA/hls/ANDROID_KATTAPPA.m3u8?hdnts=exp=1602434429~acl=/*~hmac=618751710dfe3d58a9ef241e4608f5ddfad9de62895f026fe3254e341debd8bc"
+}
+```
+
+<br><br>
+
+#### **Deploy**
+
+- Open <tt>token.tuhin</tt> & paste your token.
+- Install Dependencies 
+
+```bash
+npm install
+```
+
+- Start the server
+
+```bash
+node index.js
+```
+<br><br>
+
+### Credits 
+
+- Research by **[Manoj](https://t.me/manu855 "Manoj")**
+- Code to **[Me](https://thetuhin.com "Me")**
+<br><br><br>
+
+## Star this Repo if you Liked it ⭐⭐⭐
+
+<br><br>
+<p align="center"> <b>My Website & Social</b></p>
+<br>
+<p align="center">
+ 
+ <a href="https://tu.hin.life">
+    <img alt="Website" width="30px" src="https://firebasestorage.googleapis.com/v0/b/webtuhin.appspot.com/o/githubstatic%2Fwebsite.svg?alt=media&token=5c3ea7e0-d4f7-4566-b78a-bdee6c65f03e" />
+  </a>  
+..
+<a href="https://fb.me/jeeetpaul">
+    <img alt="Facebook" width="30px" src="https://cdn.jsdelivr.net/npm/simple-icons@3.2.0/icons/facebook.svg" />
+  </a>  
+..
+  <a href="https://www.instagram.com/jeeetpaul">
+    <img alt="Instagram" width="30px" src="https://cdn.jsdelivr.net/npm/simple-icons@3.2.0/icons/instagram.svg" />
+  </a>
+..
+  <a href="https://www.youtube.com/channel/UCa4FMtLpYcOBtjKOZgzTFNA">
+    <img alt="YouTube" width="30px" src="https://cdn.jsdelivr.net/npm/simple-icons@3.2.0/icons/youtube.svg" />
+  </a>
+..
+  <a href="https://blog.iamtuhin.ga">
+    <img alt="Blogger" width="30px" src="https://cdn.jsdelivr.net/npm/simple-icons@3.2.0/icons/blogger.svg" />
+  </a>
+  
+</p>
+
+
---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+const fs = require('fs');
+
+const gistbase = "https://prod-api-cached-2.viewlift.com/content/pages?site=aha-tv&includeContent=true&moduleOffset=0&moduleLimit=5&languageCode=default&countryCode=IN&path=";
+const moviebase = "https://prod-api.viewlift.com/entitlement/video/status?deviceType=web_browser&contentConsumption=web&id=";
+
+fs.readFile('token.tuhin', (err, data) => {
+    if (err) {
+        console.log("Error to import Token")
+    } else {
+        var token = data;
+    };
+});
+
+app.get('/', (req, res) => {
+    res.send(`Server is Running`)
+})
+
+app.get('/movie', (req, res) => {
+    let query = req.query.query;
+    var moviepath = query.replace("https://www.aha.video", "");
+    var options = {
+        'method': 'GET',
+        'url': gistbase + moviepath
+    };
+    request(options, function(error, response) {
+        if (error) {
+            console.log("Something error happened")
+        } else {
+            fs.readFile('token.tuhin', (err, data) => {
+                if (err) {
+                    console.log("Error to import Token")
+                } else {
+                    var token = data;
+                    var gistr = JSON.parse(response.body);
+                    var gistid = gistr.modules[1].contentData[0].gist.id;
+
+                    var request = require('request');
+                    var options = {
+                        'method': 'GET',
+                        'url': moviebase + gistid,
+                        'headers': {
+                            'authorization': token
+                        }
+                    };
+                    request(options, function(error, response) {
+                        if (error) {
+                            console.log("Something error happened")
+                        } else {
+                            var movied = JSON.parse(response.body);
+                            var movietitle = movied.video.gist.title;
+                            var imageurl = movied.video.gist.posterImageUrl;
+                            var qone = movied.video.streamingInfo.videoAssets.mpeg[0].renditionValue;
+                            var qonelink = movied.video.streamingInfo.videoAssets.mpeg[0].url;
+                            var qtwo = movied.video.streamingInfo.videoAssets.mpeg[1].renditionValue;
+                            var qtwolink = movied.video.streamingInfo.videoAssets.mpeg[1].url;
+                            var qthree = movied.video.streamingInfo.videoAssets.mpeg[2].renditionValue;
+                            var qthreelink = movied.video.streamingInfo.videoAssets.mpeg[2].url;
+                            var slinks = movied.video.streamingInfo.videoAssets.hlsDetail.url;
+                            res.send(`{"title":"` + movietitle + `","poster":"` + imageurl + `","download` + qone + `":"` + qonelink + `","download` + qtwo + `":"` + qtwolink + `","download` + qthree + `":"` + qthreelink + `","streaming_link":"` + slinks + `"}`)
+                        };
+                    });
+                };
+            });
+        };
+    });
+
+    res.status(200);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Content-Type', 'application/json');
+    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening')
+})
